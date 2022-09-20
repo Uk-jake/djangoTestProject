e17554ffb3df18582573690acd4a1fcb2f964345
Uk-jake <mag0225@naver.com>
2022-09-20 23:31:43 +0900

    Update 20220920

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,68 @@
 from django.shortcuts import render, HttpResponse
 
+topics = [
+    {"id": 1, "title": "Model", "body": "Model is ..."},
+    {"id": 2, "title": "View", "body": "View is ..."},
+    {"id": 3, "title": "routing", "body": "Routing is ..."}
+]
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+
+    for topic in topics:
+        ol = ol + f'<li><a href = "read/{topic["id"]}">{topic["title"]}</a></li>'
+
+    return f'''
+    <html lang="en">
+    <head>
+    <body>
+        <h1><a href = "/">Django</a></h1>
+        <ul>    
+            {ol}
+        </ul>
+        {articleTag}
+        
+        <ul>
+            <li><a href = "/create/">create</a></li>
+        </ul>
+    
+    </body>
+    </html>
+        '''
+
 def index(request):
-    return HttpResponse('Welcome!')
+
+    article = '''
+    <h2>Welcome</h2>
+    Hello Django!
+    '''
+
+    return HttpResponse(HTMLTemplate(article))
+
 
 def create(request):
-    return HttpResponse('Create!')
+    article = '''
+    <form action = "create/">
+        <p><input type = "text" name = "title" placeholder = "title"></p>
+        <p><textarea name = "body" placeholder = "body"></textarea></p>
+        <p><input type = "submit"></p>
+    </form>
+    '''
+
+
+    return HttpResponse(HTMLTemplate(article))
+
 
 def read(request, id):
-    return HttpResponse('Read :' + id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>'
+
+    return HttpResponse(HTMLTemplate(article))
+
+
+def update(request, id):
+    return HttpResponse('Update : ' + id)
